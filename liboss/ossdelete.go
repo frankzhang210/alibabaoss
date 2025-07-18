87f5a5d02dafc87491745f3ba33544ab5e462bf4
jzhang28 <frankzhang210@yahoo.com>
2025-07-17 17:19:31 -0700

    Cleanup not-used code

--- liboss/ossdelete.go
+++ liboss/ossdelete.go
@@ -2,10 +2,7 @@ package liboss
 
 import (
 	"errors"
-	"log"
 	"strings"
-
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
 func Delete(key string) error {
@@ -24,39 +21,6 @@ func Delete(key string) error {
 	if err != nil {
 		return err
 	}
-
-	return nil
-}
-
-func DeleteByPrefix(key string) error {
-
-	key = strings.TrimSpace(key)
-	if key == "" {
-		return errors.New("Prefix cannot be empty")
-	}
-
-	bucket, err := GetBucket(BucketName)
-	if err != nil {
-		return err
-	}
-
-	lor, err := bucket.ListObjects(oss.Prefix(key))
-	if err != nil {
-		return err
-	}
-	// fmt.Println("my objects prefix :", getObjectsFromResponse(lor))
-
-	objects := make([]string, len(lor.Objects))
-	for i, obj := range lor.Objects {
-		log.Println("key with same prefix ", obj.Key, i)
-		objects[i] = obj.Key
-	}
-
-	delRes, err := bucket.DeleteObjects(objects, oss.DeleteObjectsQuiet(false))
-	if err != nil {
-		return err
-	}
-	log.Println("response: ", delRes)
 
 	return nil
 }
